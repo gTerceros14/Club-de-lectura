d62b792c7f33661c9bc8b0d683b0d560bdf06ac3
gTerceros14 <114947448+gTerceros14@users.noreply.github.com>
2022-11-30 17:39:37 -0400

    Delete Admin.py

--- Admin.py
+++
@@ -1,8 +0,0 @@
-from persona import *
-class Admin(Persona):
-    def __init__(self, nombrePersona, apellidoPersona, nombreUsuario, CI, contrasenia, edad, permisoAdmin):
-        super().__init__(nombrePersona, apellidoPersona, nombreUsuario, CI, contrasenia, edad)
-        self._permisoAdmin = permisoAdmin
-    
-    def __str__(self) -> str:
-        return super().__str__() + '\nPermiso admin: ' + str(self._permisoAdmin)
