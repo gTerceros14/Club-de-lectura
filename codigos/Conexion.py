9b3cf2ed967fae210164666328d7646ee913245a
LideOP <95317741+LideOP@users.noreply.github.com>
2022-11-30 23:50:58 -0400

    Subiendo nuevas funciones

--- codigos/Conexion.py
+++ codigos/Conexion.py
@@ -3,4 +3,171 @@ class Conexion:
     def __init__(self):
         self.db = conn.DB()
 
+    def Registro(self):
+        try:
+            print("Registrar Usuario")
+            print("****************")
+            nombre=str(input("Nombre: "))
+            apellido=str(input("Apellido: "))
+            ci=int(input("Ci: "))
+            cuentaBancaria=int(input("Cuenta Bancaria: "))
+            sql="SELECT * FROM banco WHERE numeroCuenta=?"
+            parametros=(cuentaBancaria,)
+            if self.db.ejecutarConsulta(sql,parametros).fetchall()==[]:
+                print("No existe la cuenta bancaria")
+                print("crear una cuenta")
+                password=input("Password banco: ")
+                saldo=float(input("Saldo: "))
+                if saldo>=5:
+                    sql="INSERT INTO banco (numeroCuenta,password,saldo) VALUES (?,?,?)"
+                    parametros=(cuentaBancaria,password,saldo)
+                    self.db.ejecutarConsulta(sql,parametros)
+                    nomUsuario=str(input("Nombre de Usuario: "))
+                    contrasenia=str(input("Contrasenia: "))
+                    parametros=(nombre,apellido,ci,cuentaBancaria,nomUsuario,contrasenia,"Nuevo ingreso")
+                    sql="INSERT INTO usuario(nombre,apellido,ci,cuentaBancaria,nomUsuario,contrasenia,estado) VALUES(?,?,?,?,?,?,?)"
+                    self.db.ejecutarConsulta(sql,parametros)
+                    print("Usuario insertado") 
+                else:
+                    print("No pudo registrarse, el saldo debe ser mayor a 5 bs")
+                    self.Registro()
+            else:
+                nomUsuario=str(input("Nombre de Usuario: "))
+                contrasenia=str(input("Contrasenia: "))
+                parametros=(nombre,apellido,ci,cuentaBancaria,nomUsuario,contrasenia,"Nuevo ingreso")
+                sql="INSERT INTO usuario(nombre,apellido,ci,cuentaBancaria,nomUsuario,contrasenia,estado) VALUES(?,?,?,?,?,?,?)"
+                self.db.ejecutarConsulta(sql,parametros)
+                print("Usuario insertado")   
+        except:
+            print("Error al registrar Usuario")
+    def RegistroLibro(self):
+        try:
+            print("Registrar Libro")
+            print("****************")
+            nombre=str(input("Nombre del libro: "))
+            fechaPub=input("Fecha de publicacion: ")
+            autor=str(input("Autor: "))
+            editorial=str(input("Editorial: "))
+            categoria=str(input("categoria: "))
+            precio=int(input("Precio"))
+            calificacion=0
+            parametros=(nombre,fechaPub,autor,editorial,categoria,precio,calificacion)
+            sql="INSERT INTO libros(nombre,fechaPub,autor,editorial,categoria,precio,calificacion) VALUES(?,?,?,?,?,?,?)"
+            self.db.ejecutarConsulta(sql,parametros)
+            print("Libro insertado")
+        except:
+            print("error al registrar libro")
+
+    def MostrarLibro(self):
+        sql="SELECT * FROM libros"
+        resultado=self.db.ejecutarConsulta(sql).fetchall()
+        for i in range(len(resultado)):
+            sql="SELECT reseñas.id_reseña, reseñas.reseña, reseñas.calificacionPos, reseñas.calificacionNeg FROM reseñas WHERE id_libro=?"
+            parametros=(resultado[i][0],)
+            resultadoResenia=self.db.ejecutarConsulta(sql,parametros).fetchall()
+            print("Id libro", resultado[i][0])
+            print("Nombre ", resultado[i][1])
+            print("Autor ", resultado[i][2])
+            print("Precio ", resultado[i][3])
+            print("Categoria", resultado[i][4])
+            print("Editorial", resultado[i][5])
+            print("Fecha de publicacion", resultado[i][6])
+            print("Calificacion :",resultado[i][7])
+            print("")
+            if resultadoResenia!=[]:
+                for j in range(len(resultadoResenia)):
+                    print("Id resenia ",resultadoResenia[j][0])
+                    print("Resenia ",resultadoResenia[j][1])
+                    print("Calificacion positiva ",resultadoResenia[j][2])
+                    print("Calificacaion Negativa ",resultadoResenia[j][3])
+                    print("")
+            else:
+                print(" sin reseña")
+            print("="*25)
+            print("")
+            
+            
+    def ActualizarLibro(self):
+        try:
+            id=int(input("ID libro: "))
+            if self.buscarLibro(id)==True:
+                nombre = input("Nombre: ")
+                autor=input("Autor: ")
+                precio=int(input("precio: "))
+                categoria=input("Categoria: ")
+                editorial=input("Editorial: ")
+                fechaPub=input("Fecha de publicacion: ")
+                sql="UPDATE libros SET nombre=?, autor=?, precio=?, categoria=?, editorial=?,fechaPub=? WHERE id_libro=?"
+                parametros=(nombre,autor,precio,categoria,editorial,id,fechaPub)
+                self.db.ejecutarConsulta(sql,parametros)
+            else:
+                print(" No existe libro")
+        except:
+            print(" error al actualizar libro" )
+    def EliminarLibro(self):
+        try:
+            id=int(input("ID libro: "))
+            if self.buscarLibro(id)==True:
+                sql="DELETE FROM libros WHERE id_libro=?"
+                parametros=(id,)
+                self.db.ejecutarConsulta(sql,parametros)
+            else:
+                print("no existe libro")
+        except:
+            print("hubo un error ")	
+
+    # GERMAN
+    def verCalificacionResenia(self):
+        self.MostrarLibro()
     
+    def librosComprados(self,id):
+        #sql="SELECT libros.nombre,libros.autor,libros.categoria,libros.editorial,libros.fechaPub,libros.calificacion from ventas,libros WHERE ventas.id_usuario=? and ventas.id_libro=libros.id_libro"
+        sql="SELECT ventas.Nombre, ventas.autor, ventas.categoria, ventas.editorial, ventas.fechapub, ventas.precio from ventas WHERE id_usuario=? " 
+        resultado=self.db.ejecutarConsulta(sql,(id,))
+        datos=resultado.fetchall()
+        for i in range(len(datos)):
+            print("="*25)
+            print("libro # : ",i+1)
+            print("Nombre ", datos[i][0])
+            print("Autor ", datos[i][1])
+            print("Categoria", datos[i][2])
+            print("Editorial", datos[i][3])
+            print("Fecha de publicacion", datos[i][4])
+            print("Precio :",datos[i][5])
+            print("="*25)
+    
+    def verSaldo(self,cuentaBanco):
+        sql="SELECT * FROM banco WHERE numeroCuenta=?"
+        resultado=self.db.ejecutarConsulta(sql,(cuentaBanco,)).fetchall()
+        print("="*25)
+        print("Numero de cuenta: ",resultado[0][1])
+        print("Saldo disponible: ",resultado[0][3])
+        print("="*25)
+        return resultado[0][3]
+    
+    #Edil 
+    def buscarLibro(self,id):
+        sql="SELECT * FROM libros WHERE id_libro=?"
+        parametros=(id,)
+        resultado=self.db.ejecutarConsulta(sql,parametros).fetchall()
+        if resultado==[]:
+            return False
+        else: 
+            return True
+    def buscarResenia(self,id,resenia):
+        sql="SELECT * FROM reseñas WHERE id_libro=? and id_reseña=?"
+        parametros=(id,resenia)
+        resultado=self.db.ejecutarConsulta(sql,parametros).fetchall()
+        if resultado==[]:
+            return False
+        else:
+            return True
+    def UsuariosExistentes(self):
+        sql="SELECT * FROM usuario"
+        resultado=self.db.ejecutarConsulta(sql)
+        datos=resultado.fetchall()
+        return datos
+
+    
+
+
--- codigos/Login.py
+++ codigos/Login.py
@@ -9,4 +9,110 @@ class Login:
         self._admin=Admin()
         self.db = conn.DB()
 
-    
+    def ingresar(self,dato):
+
+        # FUNCIONES USUARIO
+        if dato==1:
+            if self._conexion.UsuariosExistentes():
+                datos=self._conexion.VerificarContrasenia()
+                if datos:
+                    print("A ingresado al sistema del club de libros como CLIENTE: ",datos[0][1],datos[0][2])
+                    if datos[0][7]=='Nuevo ingreso':
+                        print("NUEVO INGRESO")
+                        if self._conexion.actualizarSaldo(datos[0][4],5)==1:
+                            print("Se le otorgo 5 libros de programacion como paquete de bienvenida")
+                            sql="SELECT * FROM libros WHERE categoria=?"
+                            parametros=("Programacion",)
+                            resultado=self.db.ejecutarConsulta(sql,parametros).fetchall()
+                            for i in range(len(resultado)):
+                                sql="INSERT INTO ventas (id_usuario,id_libro,nombre,autor,precio,categoria,editorial,fechapub) VALUES (?,?,?,?,?,?,?,?)"
+                                parametros=(datos[0][0],resultado[i][0],resultado[i][1],resultado[i][2],resultado[i][3],resultado[i][4],resultado[i][5],resultado[i][6])
+                                self.db.ejecutarConsulta(sql,parametros)
+                            sql="UPDATE usuario SET estado=? WHERE id_usuario=?"
+                            parametros=("Usuario Regular",datos[0][0])
+                            self.db.ejecutarConsulta(sql,parametros)
+                        else:
+                            print("La constrasenia de la cuenta bancaria es incorrecta")
+                            self.ingresar(dato)
+                    opcion=-1
+                    while opcion!=0:
+                        print("""
+                        [1]Comprar libro
+                        [2]Reseniar
+                        [3]Calificar libro
+                        [4]Calificar resenia
+                        [5]Ver calificacion de libros y resenias
+                        [6]ver libros comprados
+                        [7]ver saldo
+                        [8]recargar saldo
+                        [0]Salir""")
+                        try:              
+                            opcion=int(input(">> "))
+                        
+                            if opcion==1:
+                                self._conexion.MostrarLibro()
+                                print("-"*35)
+                                self._conexion.venderLibro(datos[0][0],datos[0][4])
+                            if opcion==2:
+                                self._conexion.reseniarLibro(datos[0][0])
+                            if opcion==3:
+                                self._conexion.calificarLibro(datos[0][0])
+                            if opcion==4:
+                                self._conexion.calificarResenia()
+                            if opcion==5:
+                                self._conexion.verCalificacionResenia()
+                            if opcion==6:
+                                self._conexion.librosComprados(datos[0][0])
+                            if opcion==7:
+                                self._conexion.verSaldo(datos[0][4])
+                            if opcion==8:
+                                self._conexion.recargarSaldo(datos[0][4])
+                            elif opcion==0:
+                                pass
+                        except:
+                            print("opcion invalida")
+                else:
+                    print("La contrasenia o usuario es incorrecta: ")
+                    self.ingresar(dato)
+            else:
+                print("NO hay usuarios registrados")
+
+        elif dato==2:
+            usuarioAdmin=input("Ingrese usuario: ")
+            contraseniaAdmin=input("Ingrese contrasenia: ")
+            sql="SELECT * FROM administrador WHERE usuarioAdmin =? And contraseniaAdmin=?"
+            parametros=(usuarioAdmin,contraseniaAdmin)
+            resultado=self.db.ejecutarConsulta(sql,parametros)
+            if resultado.fetchall():
+                print("A ingresado al sistema del club de libros como ADMIN: ")
+                opcion=-1
+                while opcion!=0:
+                    print("""
+                    [1]Agregar libro
+                    [2]Eliminar libro
+                    [3]Actualizar contenido del libro
+                    [4]Ver libros
+                    [5]Ver usuarios registrados
+                    [0]Salir""")
+                    try:              
+                        opcion=int(input(">> "))
+                    
+                        if opcion==1:
+                            self._conexion.RegistroLibro()
+                        if opcion==2:
+                            self._conexion.EliminarLibro()
+                        if opcion==3:
+                            self._conexion.ActualizarLibro()
+                        if opcion==4:
+                            self._conexion.MostrarLibro()
+                        if opcion==5:
+                            self._conexion.MostrarUsuario()
+
+                        elif opcion==0:
+                            pass
+                    except:
+                        print("Opcion invalida")
+            else:
+                print("La contrasenia o usuario es incorrecta: ")
+                self.ingresar(dato)
+            
